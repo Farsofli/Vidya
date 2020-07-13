443570ba8cd264e9daa799afbdd8c0b07dcc4e99
FidaBucket <fida@geniewords.com>
2020-07-13 13:33:53 +0200

    Tilesets and scenes
    Tiles can now be placed isometrically

--- Player.gd
+++ Player.gd
@@ -2,3 +2,21 @@ extends KinematicBody2D
 
 var score : int = 0
 var speed : int = 201
+
+var vel : Vector2 = Vector2()
+
+onready var sprite : Sprite = get_node("Player_down")
+
+#loaded every 60 sec
+func _physics_process(delta):
+	
+	vel.x = 0
+	
+	if Input.is_action_pressed("move_left"):
+		vel.x -= speed
+		
+	if Input.is_action_pressed("move_right"):
+		vel.x +=speed
+		
+	vel = move_and_slide(vel, Vector2.UP)
+	
